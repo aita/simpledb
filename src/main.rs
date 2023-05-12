fdf0a5c207a801b744fb1fa08856edb6f92518ea
Ryo Aita <792803+aita@users.noreply.github.com>
2023-05-12 21:19:20 +0900

    World's Simplest SQL Compiler and Virtual Machine

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use thiserror::Error;
 
 fn print_prompt() {
     print!("db > ");
@@ -9,19 +10,73 @@ fn read_input(buf: &mut String) -> io::Result<usize> {
     io::stdin().read_line(buf)
 }
 
+#[derive(Error, Debug)]
+enum MetaCommandError {
+    #[error("unrecognized command '{0}'")]
+    UnrecognizedCommand(String),
+}
+
+fn db_meta_command(input: &str) -> Result<(), MetaCommandError> {
+    match input {
+        ".exit" => {
+            std::process::exit(0);
+        }
+        _ => Err(MetaCommandError::UnrecognizedCommand(input.to_string())),
+    }
+}
+
+#[derive(Debug)]
+enum Statement {
+    Insert,
+    Select,
+}
+
+#[derive(Error, Debug)]
+enum PrepareError {
+    #[error("unrecognized keyword at start of '{0}'")]
+    UnrecognizedKeyword(String),
+}
+
+fn prepare_statement(input: &str) -> Result<Statement, PrepareError> {
+    if input.starts_with("insert") {
+        Ok(Statement::Insert)
+    } else if input.starts_with("select") {
+        Ok(Statement::Select)
+    } else {
+        Err(PrepareError::UnrecognizedKeyword(input.to_string()))
+    }
+}
+
+fn execute_statement(statement: Statement) {
+    match statement {
+        Statement::Insert => println!("This is where we would do an insert."),
+        Statement::Select => println!("This is where we would do a select."),
+    }
+}
+
 fn main() {
     loop {
         print_prompt();
 
         let mut input: String = String::new();
         read_input(&mut input).expect("Failed to read input");
+        let input = input.trim();
 
-        match input.trim() {
-            ".exit" => {
-                break;
+        if input.starts_with(".") {
+            match db_meta_command(input) {
+                Ok(_) => continue,
+                Err(e) => {
+                    println!("{}", e);
+                }
             }
-            _ => {
-                println!("Unrecognized command '{}'.", input.trim());
+        }
+
+        match prepare_statement(input) {
+            Ok(statement) => {
+                execute_statement(statement);
+            }
+            Err(e) => {
+                println!("{}", e);
             }
         }
     }
